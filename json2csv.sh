aecca3fdde1df35722dc8040e6d8ab9b7633a271
CG Remuzzi <carlosremuzzi@gmail.com>
2020-02-17 19:08:59 -0400

    fixing column names

--- json2csv.sh
+++ json2csv.sh
@@ -10,9 +10,8 @@ mkdir -p $TARGET_DIR
 convert() {
     output_file="${1}.csv"
     echo "converting ${1} to ${output_file}"
-    echo "id,name,code,region_id,latitude,longitude,rank,score" > ${output_file}
-    jq -r '. | map([.passport.id, .passport.name, .passport.code, .passport.region_id, .passport.latitude, .passport.longitude, .passport.rank, .passport.score ] | join(", ")) | join("\n")' ${1} >> ${output_file}
-    #sleep 2
+    echo "ID,NAME,CODE,REGION_ID,LATITUDE,LONGITUDE,RANK,SCORE" > ${output_file}
+    jq -r '. | map([.passport.id, .passport.name, .passport.code, .passport.region_id, .passport.latitude, .passport.longitude, .passport.rank, .passport.score ] | join(",")) | join("\n")' ${1} >> ${output_file}
 }
 
 for i in $SOURCE_DIR/*.json
