ea0ab5e2215f171299211df0003ebc9b59954261
CG Remuzzi <carlosremuzzi@gmail.com>
2020-02-18 12:00:51 -0400

    changing csv column header for passport code

--- json2csv.sh
+++ json2csv.sh
@@ -3,14 +3,11 @@
 # this script helps you convert the retrieved json data to csv
 
 SOURCE_DIR=${PWD}/data
-TARGET_DIR=${PWD}/csv
-
-mkdir -p $TARGET_DIR 
 
 convert() {
     output_file="${1}.csv"
     echo "converting ${1} to ${output_file}"
-    echo "ID,NAME,CODE,REGION_ID,LATITUDE,LONGITUDE,RANK,SCORE" > ${output_file}
+    echo "ID,NAME,ISO_3166,REGION_ID,LATITUDE,LONGITUDE,RANK,SCORE" > ${output_file}
     jq -r '. | map([.passport.id, .passport.name, .passport.code, .passport.region_id, .passport.latitude, .passport.longitude, .passport.rank, .passport.score ] | join(",")) | join("\n")' ${1} >> ${output_file}
 }
 
